0b9e856346b719c68cfe1ebbaf981e1e35f22d30
NoShare16 <alex.wirschke14@gmail.com>
2023-08-22 17:19:42 +0200

    added picture,missing link and animation wh. hover

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -16,28 +16,38 @@ const Projects = () => {
         />
 
 
-        <div className='relative z-10 pt-3 md:pt-0 md:top-1/4 lg:top-1/3 max-w-7xl mx-auto flex justify-between'>
+        <div className='relative z-10 pt-3 md:pt-0 md:top-1/4 lg:top-1/3 max-w-7xl mx-auto flex flex-col lg:flex-row'>
             <div className='max-w-2xl px-4 flex flex-col gap-8 md:max-xl:bg-black/60 md:max-xl:rounded-lg'>
                 <h1 className='-ml-2 text-5xl md:text-7xl font-semibold text-white'>
                     Meine Projekte<span className='md:text-purple-500 md:text-7xl'>.</span>
                 </h1>
-                <p className='text-white text-lg max-w-xl'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore beatae sequi modi nisi suscipit amet quibusdam ullam. Dolorem non est atque veniam rerum voluptatum praesentium, quasi consequuntur distinctio repudiandae tempore.
+                <p className='text-white text-base max-w-xl md:text-xl'>
+                Seit Beginn meiner Programmier-Reise habe ich viel gelernt und eigene Projekte entwickelt. Ich arbeite selbstständig, liebe neue Herausforderungen und wachse mit jedem Code, den ich schreibe. Hier sind einige meiner Werke.
                 </p>
+
+
             </div>
 
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-2 gap-4 m-4">
             <div>
-                <Image src="/assets/images/hoodedHacker.jpg" alt="Description for image 1" width={500} height={500} />
+                <a href="/">
+                    <Image src="/assets/images/HandLens.png" alt="Description for image 1" width={500} height={500} className='rounded-xl'/>
+                </a>
             </div>
             <div>
-                <Image src="/assets/images/hoodedHacker.jpg" alt="Description for image 2" width={500} height={500} />
+                <a href="/">
+                    <Image src="/assets/images/AiMakingMoney.jpg" alt="Description for image 1" width={500} height={500} className='rounded-xl'/>
+                </a>
             </div>
             <div>
-                <Image src="/assets/images/hoodedHacker.jpg" alt="Description for image 3" width={500} height={500} />
+                <a href="/">
+                    <Image src="/assets/images/PersonTalkingAi.jpg" alt="Description for image 1" width={500} height={500} className='rounded-xl'/>
+                </a>
             </div>
             <div>
-                <Image src="/assets/images/hoodedHacker.jpg" alt="Description for image 4" width={500} height={500} />
+                <a href="/">
+                    <Image src="/assets/images/TeachingAi.jpg" alt="Description for image 1" width={500} height={500} className='rounded-xl'/>
+                </a>
             </div>
         </div>
         </div>
